7b19c28daca982acb891de871a54621a46316588
Raul Striglio <raul.striglio@gmail.com>
2016-09-02 12:17:46 -0300

    merged with stashed files

--- app/src/main/java/base/apptrailers/mobile/globant/com/moviesapp/Activities/SplashActivity.java
+++ app/src/main/java/base/apptrailers/mobile/globant/com/moviesapp/Activities/SplashActivity.java
@@ -1,13 +1,19 @@
-package base.apptrailers.mobile.globant.com.moviesapp;
+package base.apptrailers.mobile.globant.com.moviesapp.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import base.apptrailers.mobile.globant.com.moviesapp.R;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
